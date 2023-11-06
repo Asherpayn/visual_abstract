982101739e5972165fa61eb9d9215ae1df2fc333
Asherpayn <asherpayn@gmail.com>
2023-11-06 18:24:47 +0000

    stuff

--- Tkinter.py
+++ Tkinter.py
@@ -1,6 +1,6 @@
-from tkinter import *
+import tkinter as Tk
 window = Tk()
 window.title('Tkinter window example')
-c = Canvas(window, width=800, height=500, bg='lightblue')
+c = Tk.Canvas(window, width=800, height=500, bg='lightblue')
 c.create_text(400, 250, text='This is an example of a Tkinter window', fill='black')
 c.pack()
